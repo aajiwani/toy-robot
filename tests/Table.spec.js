8706bb59ab240ebbb2c96fc68ca93ea92efde9ff
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2018-03-15 14:44:58 +0800

    Adds more table specs

--- tests/Table.spec.js
+++ tests/Table.spec.js
@@ -1,18 +1,9 @@
 "use strict";
 
 import Table from "@app/src/Table";
-
 import { expect } from "chai";
 
-describe("Litmus test", () => {
-	describe("1 = 1", () => {
-		it("should return true", () => {
-			expect(1).to.be.eq(1);
-		});
-	});
-});
-
-describe("Table making test", () => {
+describe("Table creation tests", () => {
 	describe("Create 5x5 table", () => {
 		it("should respond to size as 5x5", () => {
 			let table = new Table(5, 5);
@@ -23,6 +14,19 @@ describe("Table making test", () => {
 			expect(table.Size)
 				.to.have.property("y")
 				.eq(5);
+		});
+	});
+
+	describe("Create 6x10 table", () => {
+		it("should respond to size as 6x10", () => {
+			let table = new Table(6, 10);
+			expect(table.Size).to.be.an("object");
+			expect(table.Size)
+				.to.have.property("x")
+				.eq(6);
+			expect(table.Size)
+				.to.have.property("y")
+				.eq(10);
 		});
 	});
 });

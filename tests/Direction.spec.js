7a315437be6934c9b487ff15fbea32c02b11919d
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2018-03-16 21:00:20 +0800

    Improves on Direction tests

--- tests/Direction.spec.js
+++ tests/Direction.spec.js
@@ -1,17 +1,31 @@
 "use strict";
 
 import Direction from "@app/src/Helpers/Direction";
-import { expect } from "chai";
+import {expect} from "chai";
 
 describe("Parsing of Direction", () => {
-	it("should parse EAST, WEST, NORTH, SOUTH correctly case-insensetively", () => {
-		expect(Direction.Parse("east")).to.be.eq(Direction.EAST);
-		expect(Direction.Parse("EAST")).to.be.eq(Direction.EAST);
-		expect(Direction.Parse("west")).to.be.eq(Direction.WEST);
-		expect(Direction.Parse("WeSt")).to.be.eq(Direction.WEST);
-		expect(Direction.Parse("south")).to.be.eq(Direction.SOUTH);
-		expect(Direction.Parse("sOuTh")).to.be.eq(Direction.SOUTH);
-		expect(Direction.Parse("noRTH")).to.be.eq(Direction.NORTH);
-		expect(Direction.Parse("North")).to.be.eq(Direction.NORTH);
-	});
+  it("should parse EAST, WEST, NORTH, SOUTH correctly case-insensetively", () => {
+    expect(Direction.Parse("east")).to.be.eq(Direction.EAST);
+    expect(Direction.Parse("EAST")).to.be.eq(Direction.EAST);
+    expect(Direction.Parse("west")).to.be.eq(Direction.WEST);
+    expect(Direction.Parse("WeSt")).to.be.eq(Direction.WEST);
+    expect(Direction.Parse("south")).to.be.eq(Direction.SOUTH);
+    expect(Direction.Parse("sOuTh")).to.be.eq(Direction.SOUTH);
+    expect(Direction.Parse("noRTH")).to.be.eq(Direction.NORTH);
+    expect(Direction.Parse("North")).to.be.eq(Direction.NORTH);
+  });
+
+  it("shouldn't parse other strings to form direction", () => {
+    expect(() => {
+      Direction.Parse("Bogus");
+    }).to.throw("Given string is not a direction");
+
+    expect(() => {
+      Direction.Parse("EA ST");
+    }).to.throw("Given string is not a direction");
+
+    expect(() => {
+      Direction.Parse("WESTa");
+    }).to.throw("Given string is not a direction");
+  });
 });

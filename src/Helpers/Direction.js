6ac164b592fd6d4af5c42e31eb922044ca63bead
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2018-03-16 14:31:25 +0800

    Enhances direction to parse as well now

--- src/Helpers/Direction.js
+++ src/Helpers/Direction.js
@@ -1,6 +1,24 @@
-export default {
+import _ from "lodash";
+
+const Direction = {
 	NORTH: "NORTH",
 	SOUTH: "SOUTH",
 	EAST: "EAST",
 	WEST: "WEST",
+	Parse: text => {
+		switch (_.lowerCase(text)) {
+			case "north":
+				return this.NORTH;
+			case "south":
+				return this.SOUTH;
+			case "east":
+				return this.EAST;
+			case "west":
+				return this.WEST;
+			default:
+				return null;
+		}
+	},
 };
+
+export default Object.freeze(Direction);
